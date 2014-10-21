4f83162e003c7d95d58937d2fbec12e5f612d589
damith senanayake <dracusds123@gmail.com>
2014-10-21 12:37:45 +0530

    first commit of architecture outline

---
+++ io/filehandler.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 21, 2014
+
+@author: gilgamesh
+'''
+
+class MyClass():
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
